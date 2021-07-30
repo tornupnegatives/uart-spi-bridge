c1477be8c52b09b560c17f7e2be175e9ebe4e09b
Joseph Bellahcen <tornupnegatives@gmail.com>
2021-07-30 10:30:15 -0500

    Updated state machine

--- uart_spi_bridge_tb.sv
+++ uart_spi_bridge_tb.sv
@@ -36,69 +36,41 @@ module uart_spi_bridge_tb;
         tx_data    = 8'b10110101;
         rx_data    = 8'b11011100;
         
-
         reset;
 
-        $display("Starting transmission: %b", tx_data);
+        $display("Transmitting to UART: %b\n", tx_data);
+
+        $display("Asserting start bit...");
         @(posedge DUT.UART.BD.r_rising_edge)
             #t_in i_rx = 'h0;
 
+        $display("Sending data packet...");
         for (int i = 0; i < 8; i++)
-            @(posedge DUT.UART.BD.r_rising_edge) begin
+            @(posedge DUT.UART.BD.r_rising_edge)
                 #t_in i_rx = tx_data[i];
-                $display("UART_TX x%h", tx_data[i]);
-            end
 
-        $display("\nSending stop bits");
+        $display("Sending stop bits...\n");
         @(posedge DUT.UART.BD.r_rising_edge)
             #t_in i_rx = 'h1;
 
-        $display("Sending over SPI...");
-        for (int i = 7; i >= 0; i--)
-            @(posedge o_sclk) begin
-                i_cipo = rx_data[7-i];
-                #t_out begin 
-                        $display("SPI_TX x%h", o_copi);
+        $display("Transmitting UART packet over SPI...");
+        for (int i = 7; i >= 0; i--) begin
+            i_cipo = rx_data[7-i];
+            @(posedge o_sclk)
+                #t_out assert(o_copi == tx_data[i]) else
+                       $fatal(1, "SPI TX error");
+        end
 
-                        assert(o_copi == tx_data[i]) else
-                        $fatal(1, "SPI TX error");
-                end
-            end
-
-        $display("Waiting for UART start bit...");
+        $display("Waiting for UART to transmit SPI response...");
         @(posedge DUT.UART.BD.r_rising_edge);
         @(posedge DUT.UART.BD.r_rising_edge)
             #t_out assert(o_tx == 'h0);
 
-        $display("Transmitting UART...");
+        $display("Transmitting SPI response over UART...\n");
         for (int i = 0; i < 8; i++)
-            @(posedge DUT.UART.BD.r_rising_edge) begin
-                #t_out begin 
-                        $display("UART_TX x%h", o_tx);
-
-                        //assert(o_tx == rx_data[i]) else
-                        //$fatal(1, "UART TX error");
-                end
-            end
-
-        /*
-        for (int i = 0; i < 8; i++)
-            @(posedge DUT.UART.BD.r_rising_edge) begin
-                #t_in i_rx = tx_data[i];
-                $display("UART_TX x%h", tx_data[i]);
-            end
-            */
-
-        /*
-        $display("Waiting for SPI to finish...");
-        repeat (3) @(posedge i_clk)
-            i_cipo = 'h0;
-        while (o_ready);
-
-        $display("Waiting for UART TX...");
-        @(posedge i_clk);
-        while(~o_ready);
-        */
+            @(posedge DUT.UART.BD.r_rising_edge)
+                #t_out assert(o_tx == rx_data[7-i]) else
+                       $fatal(1, "UART TX error");
 
         $display("Simulation finish");
         $finish;
